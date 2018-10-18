c71706dedbe19ba6f091124e60954ba6f25fb7ae
joneseyit <joneseyit@protonmail.com>
2018-10-18 21:11:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 # Add  code here!
 prime?(num)
-  test_set = (1...num).to_a
-  cur_status = false
+  test_set = (2...num).to_a
+  cur_status = true
   test_set.each do |el|
-    if num % el 
+    if num % el == 0
+      cur_status = false
+    end
   end
+  cur_status
 end
